cbad5aa0a21fdd2845e516d2eafb2ace15727b8f
he ping <847050412@qq.com>
2020-12-24 12:49:34 +0800

    update

--- config/dev.env.js
+++ config/dev.env.js
@@ -5,6 +5,6 @@ const prodEnv = require('./prod.env')
 //注：开发环境修改变量后重启服务才会生效
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  API_URL: '"http://127.0.0.1:9500"',
-  WEB_SOCKET_URL: '"http://127.0.0.1:9502"',
+  API_URL: '"https://hyperf-chat.jayjay.cn"',
+  WEB_SOCKET_URL: '"wss://hyperf-chat.jayjay.cn"',
 });
--- config/prod.env.js
+++ config/prod.env.js
@@ -3,8 +3,8 @@
 //注：生产环境修改变量后重新编译项目才会生效
 module.exports = {
   NODE_ENV: '"production"',
-  API_URL: '"http://127.0.0.1:9500"',
-  WEB_SOCKET_URL: '"ws://127.0.0.1:9502/"',
+  API_URL: '"https://hyperf-chat.jayjay.cn"',
+  WEB_SOCKET_URL: '"wss://hyperf-chat.jayjay.cn"',
 
   // 网站名称
   WEBSITE_NAME: '"HyperfIM"',
